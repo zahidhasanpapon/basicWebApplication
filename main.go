23bb64b7e1f3cf517daceb77b8cc8a08bddd2fae
Zahid Hasan <pzahidhasan@outlook.com>
2022-06-18 00:39:05 +0600

    basic listen and serve

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		n, err := fmt.Fprintf(writer, "Hello, World!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
 }
